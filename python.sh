b5f534535f58b2d2759e7f5be85ca82bfd1865ae
Kaikaikaifang <2837968358@qq.com>
2024-03-16 16:50:03 +0800

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+source ~/.zshrc
+
+pyenv -v
