e035cd8840775f17de5d2d3e25a34e59845e286a
kaikai <2837968358@qq.com>
2024-03-16 17:00:30 +0800

    feat: go

---
+++ go.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install bison
+
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
+source /home/kakai/.gvm/scripts/gvm
